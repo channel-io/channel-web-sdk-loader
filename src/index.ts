73ed327b13dc1aea9f3c3172acb39a24bd5b2638
jeff-tbd <wodlf30426@gmail.com>
2023-09-23 12:23:32 +0900

    fix(init): change method name(int -> loadScript)
    add annotation for loadScript
    
    BREAKING CHANGE: change method name (init -> loadScript)

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,10 @@ interface Profile {
 
 type Appearance = 'light' | 'dark' | 'system' | null;
 
-function init() {
+/**
+ * Load the Channel SDK script.
+ */
+function loadScript() {
   (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.")}var ch:IChannelIO=function(){ch.c?.(arguments)};ch.q=[];ch.c=function(args){ch.q?.push(args)};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x)}}if(document.readyState==="complete"){l()}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l)}})();
 }
 
@@ -310,7 +313,7 @@ function setAppearance(appearance: Appearance) {
 }
 
 export default {
-  init,
+  loadScript,
   boot,
   shutdown,
   showMessenger,
