4d93bf8047857ac7765ece8e8c8f0ee9574e168c
jeff-tbd <wodlf30426@gmail.com>
2023-09-27 15:58:05 +0900

    refactor(loadscript): remove minify & add annotation
    annoation for not supporting SSR (only on browser)

--- src/index.ts
+++ src/index.ts
@@ -95,10 +95,45 @@ export interface Profile {
 export type Appearance = 'light' | 'dark' | 'system' | null;
 
 /**
- * Load the Channel SDK script.
+ * - Load the Channel SDK script.
+ * - This method is only executable on browser.
+ * - You can’t execute this method on the server-side.
  */
 export function loadScript() {
-  (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.")}var ch:IChannelIO=function(){ch.c?.(arguments)};ch.q=[];ch.c=function(args){ch.q?.push(args)};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x)}}if(document.readyState==="complete"){l()}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l)}})();
+  (function() {
+    var w = window;
+    if (w.ChannelIO) {
+      return w.console.error('ChannelIO script included twice.');
+    }
+    var ch: IChannelIO = function() {
+      ch.c?.(arguments);
+    };
+    ch.q = [];
+    ch.c = function(args) {
+      ch.q?.push(args);
+    };
+    w.ChannelIO = ch;
+    function l() {
+      if (w.ChannelIOInitialized) {
+        return;
+      }
+      w.ChannelIOInitialized = true;
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+      var x = document.getElementsByTagName('script')[0];
+      if (x.parentNode) {
+        x.parentNode.insertBefore(s, x);
+      }
+    }
+    if (document.readyState === 'complete') {
+      l();
+    } else {
+      w.addEventListener('DOMContentLoaded', l);
+      w.addEventListener('load', l);
+    }
+  })();
 }
 
 /** @see https://developers.channel.io/docs/web-boot-option */
